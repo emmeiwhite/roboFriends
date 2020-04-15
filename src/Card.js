bbf4fefc3eb42ebd9e26100a226a6bfeafe9ff51
emmeiwhite <emmeiwhite@gmail.com>
2020-04-15 22:00:27 +0530

    Created a Card component and reused it 5 times - The Re-usuablility in Action

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = ({ name, id, email }) => {
+  return (
+    <div className="card-item">
+      <img src={`https://robohash.org/${id}?200x200`} alt="Cartoon Robot" />
+      <h1>{name}</h1>
+      <p>{email}</p>
+    </div>
+  );
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Card from "./Card";
+import { robots } from "./robots";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="robots-wrapper">
+      <Card
+        className="card-item"
+        id={robots[0].id}
+        name={robots[0].name}
+        email={robots[0].email}
+      />
+      <Card
+        className="card-item"
+        id={robots[1].id}
+        name={robots[1].name}
+        email={robots[1].email}
+      />
+      <Card
+        className="card-item"
+        id={robots[2].id}
+        name={robots[2].name}
+        email={robots[2].email}
+      />
+      <Card
+        className="card-item"
+        id={robots[3].id}
+        name={robots[3].name}
+        email={robots[3].email}
+      />
+      <Card
+        className="card-item"
+        id={robots[4].id}
+        name={robots[4].name}
+        email={robots[4].email}
+      />
+      <Card
+        className="card-item"
+        id={robots[5].id}
+        name={robots[5].name}
+        email={robots[5].email}
+      />
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
