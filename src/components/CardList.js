2280ce35b80463eafdd95f410193137d81915123
emmeiwhite <emmeiwhite@gmail.com>
2020-04-17 19:57:33 +0530

    Boundary Condition with componentDidCatch()

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,9 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
+  if (true) {
+    throw new Error("NOOOOOOOOOO!");
+  }
   const cardsArray = robots.map((card) => {
     return (
       <Card id={card.id} name={card.name} email={card.email} key={card.id} />
---
+++ src/containers/ErrorBoundary.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  // This life cycle method introduced in React-16 acts like a try-catch block
+  componentDidCatch(error, info) {
+    console.log("componentDidCatch method was called");
+    this.setState({
+      hasError: true,
+    });
+  }
+  render() {
+    if (this.state.ErrorBoundary) {
+      {
+        console.log("It's an Error !!!");
+      }
+      return <h1>Oooops. This is not good!</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
