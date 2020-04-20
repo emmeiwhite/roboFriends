8bea705a28ef330347d85167a190e6f08f7cf95a
emmeiwhite <emmeiwhite@gmail.com>
2020-04-20 21:19:47 +0530

    Updates

---
+++ precache-manifest.6434d12b09d33bcf677e9c87e85d2475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4540230b75e494d0a63caf3b5ff2b76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "340ab82e5ad301117297",
+    "url": "/static/css/main.578a5e9c.chunk.css"
+  },
+  {
+    "revision": "fe8c5e9cfb33b786110e",
+    "url": "/static/js/2.ab0e463c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab0e463c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340ab82e5ad301117297",
+    "url": "/static/js/main.cabcda6a.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  }
+]);
